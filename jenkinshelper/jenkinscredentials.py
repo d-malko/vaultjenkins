205ca8bf3a4439c5b4110313783f952472978589
lionitesh10 <lionitesh10@gmail.com>
2024-03-17 16:58:39 +0545

    Added if credentials exists

--- jenkinshelper/jenkinscredentials.py
+++ jenkinshelper/jenkinscredentials.py
@@ -7,8 +7,14 @@ class JenkinsAuth:
         self.jenkinsUrl=config['jenkinsUrl']
         self.jenkinsUser=config['jenkinsUsername']
         self.jenkinsPassword=config['jenkinsPassword']
+        self.jenkinsFolder=config['foldername']
+        self.jenkinscredsId=config['credsId']
         self.jenkinsServer=jenkins.Jenkins(self.jenkinsUrl, username=self.jenkinsUser, password=self.jenkinsPassword)    
     
+    def credExists(self):
+        print(self.jenkinsServer.credential_exists(self, self.jenkinscredsId, self.jenkinsFolder))
+        return True
+
     def createRoleCredentials(self,credId,credDesc,roleId,secretId,path,folder_name):  
         newConfig=f'''
             <com.datapipe.jenkins.vault.credentials.VaultAppRoleCredential plugin="hashicorp-vault-plugin@364.vf5d54b_3dc313">
@@ -24,11 +30,11 @@ class JenkinsAuth:
               <path>{path}</path>
             </com.datapipe.jenkins.vault.credentials.VaultAppRoleCredential>
           '''
-        try:
-            result=self.jenkinsServer.create_credential(folder_name,newConfig)
-            return credId
-        except:
-            print("There is some error while creating a app role credentials")
+        # try:
+        result=self.jenkinsServer.create_credential(folder_name,newConfig)
+        return credId
+        # except:
+        #     print("There is some error while creating a app role credentials")
 
     def updateJenkinsCredApp(self,credId,credDesc,roleId,secretId,path,folder_name):
         reConfig=f'''
@@ -47,7 +53,14 @@ class JenkinsAuth:
           '''
         self.jenkinsServer.reconfig_credential(folder_name,reConfig)
         print(self.jenkinsServer.get_credential_config(roleId,folder_name))
-
+      
+    def runner(self,credId,credDesc,roleId,secretId,path,folder_name):
+        if self.credExists():
+            self.updateJenkinsCredApp(credId,credDesc,roleId,secretId,path,folder_name)
+            print("Updated")
+        else:
+            self.createRoleCredentials(credId,credDesc,roleId,secretId,path,folder_name)
+            print("Created")
     def __str__(self):
         user = self.jenkinsServer.get_whoami()
         version = self.jenkinsServer.get_version()
--- runner.py
+++ runner.py
@@ -1,9 +1,12 @@
 from config.loader import getConfig
 from vault.credentials import Vault
+from jenkinshelper.jenkinscredentials import JenkinsAuth
 
 config=getConfig()
 
 vault=Vault(config['vault'])
 print(vault)
-role_id,secret_id=vault.getCredentials('terraform')
+role_id,secret_id=vault.getCredentials('terraform-2')
+jenkins=JenkinsAuth(config['jenkins'])
+jenkins.runner(config['jenkins']['credsId'],config['jenkins']['credsId'],role_id,secret_id,'approle',config['jenkins']['foldername'])
 
